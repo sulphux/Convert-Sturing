85ae9ee472ddd8d259e5b90435d68f10093432b8
Sulphux <xD>
2013-03-06 13:55:31 +0100

    Dziala

---
+++ conventer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Podaj sciezke pliku\n";
+
+//	string path;
+//	cin >> path;
+	
+
+	string line;
+	char state = '\0';
+
+	ofstream fi ;
+	fi.open("multi3.txt");
+	
+	ifstream file;
+	file.open( "multi2.txt", ios::in );
+
+	fi << "test\n";
+	if (file.good())
+	{
+		while (!file.eof())
+		{
+			if ( getline( file, line ) ) {
+			
+			if (line.at(1) != state)
+			{
+				state = line.at(1);
+				fi << state << " :\n";
+			}
+			
+			fi << line.at(0) << " " << line.at(4) << " " << line.at(6) << " " << line.at(5) << "\n";
+			
+			cout << line << " " << state <<  "\n";}
+		}
+		file.close();
+	}
+	fi.close();
+	return 0;
+}
