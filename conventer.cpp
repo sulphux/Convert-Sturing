664e3399732d6f538a0c1204340f0823c458e437
Bogdan Cichy <sulphux@gmail.com>
2013-03-07 21:01:44 +0100

    Add a little bit of changes.

--- conventer.cpp
+++ conventer.cpp
@@ -6,36 +6,30 @@ using namespace std;
 
 int main()
 {
-	cout << "Podaj sciezke pliku\n";
-
-//	string path;
-//	cin >> path;
-	
 
 	string line;
 	char state = '\0';
 
 	ofstream fi ;
 	fi.open("multi3.txt");
-	
+
 	ifstream file;
 	file.open( "multi2.txt", ios::in );
 
-	fi << "test\n";
 	if (file.good())
 	{
 		while (!file.eof())
 		{
 			if ( getline( file, line ) ) {
-			
+
 			if (line.at(1) != state)
 			{
 				state = line.at(1);
-				fi << state << " :\n";
+				fi << state << ":\n";
 			}
-			
+
 			fi << line.at(0) << " " << line.at(4) << " " << line.at(6) << " " << line.at(5) << "\n";
-			
+
 			cout << line << " " << state <<  "\n";}
 		}
 		file.close();
